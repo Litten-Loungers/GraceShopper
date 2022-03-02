15ce0213db8465e5ae1b6d189dd45529a8b15d8f
Konstantin Balakirev <kbalakirev@gmail.com>
2022-03-02 14:15:01 -0600

    routes first run

--- server/api/products.js
+++ server/api/products.js
@@ -1,21 +1,49 @@
 const router = require('express').Router();
+const res = require('express/lib/response');
 const {
   models: { Product },
 } = require('../db');
 module.exports = router;
 
+//GET all products
 router.get('/', async (req, res, next) => {
   try {
-    const products = await Product
-      .findAll
-      // {
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      //   attributes: ['id', 'username'],
-      // }
-      ();
+    const products = await Product.findAll();
     res.json(products);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//POST single product
+router.post('/', async (req, res, next) => {
+  try {
+    res.status(201).send(await Product.create(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+
+//DELETE single product
+router.delete('/:productId', async (req, res, next) => {
+  const id = Number(req.params.productId);
+  try {
+    await Product.destroy({
+      where: {
+        id: id,
+      },
+    });
+    res.send(200);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//GET single product
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const product = await User.findByPk(req.params.productId);
+    res.json(product);
   } catch (err) {
     next(err);
   }
--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,8 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
+const router = require('express').Router();
+const {
+  models: { User },
+} = require('../db');
+module.exports = router;
 
 router.get('/', async (req, res, next) => {
   try {
@@ -8,10 +10,21 @@ router.get('/', async (req, res, next) => {
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
+      attributes: ['id', 'username'],
+    });
+    res.json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
+
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId, {
+      attributes: ['id', 'username'],
+    });
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
