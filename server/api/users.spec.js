313312da5e6606d3451129b3cc855e946f64e660
Lucas McGill <lmlucasmc8@gmail.com>
2022-03-02 17:40:20 -0500

    user tests passing

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -1,35 +1,34 @@
 /* global describe beforeEach it */
 
-const {expect} = require('chai')
-const request = require('supertest')
-const { db, models: { User } } = require('../db')
-const seed = require('../../script/seed');
-const app = require('../app')
+const { expect } = require("chai");
+const request = require("supertest");
+const {
+  db,
+  models: { User },
+} = require("../db");
+const seed = require("../../script/seed");
+const app = require("../app");
 
-describe('User routes', () => {
-  beforeEach(async() => {
+describe("User routes", () => {
+  beforeEach(async () => {
     await seed();
-  })
+  });
 
-  describe.only('/api/users/', () => {
+  describe.only("/api/users/", () => {
+    it("GET /api/users", async () => {
+      const res = await request(app).get("/api/users").expect(200);
 
-    it('GET /api/users', async () => {
-      const res = await request(app)
-        .get('/api/users')
-        .expect(200)
-
-      expect(res.body).to.be.an('array');
+      expect(res.body).to.be.an("array");
       expect(res.body.length).to.equal(8);
-    })
+    });
 
-    it('GET /api/users/:id', async () => {
-      const res = await request(app)
-        .get('/api/users/2')
-        .expect(200)
+    it("GET /api/users/:id", async () => {
+      const user = await User.findByPk(3);
+      const res = await request(app).get("/api/users/3").expect(200);
 
-      expect(res.body).to.be.an('object');
-      expect(res.body).to.equal(await User.findByPk(2, { attributes: ['id', 'username']}))
-    })
+      expect(res.body).to.be.an("object");
+      expect(res.body.username).to.equal(user.dataValues.username);
+    });
 
     // it('POST /api/users/', async () => {
     //   const res = await request(app)
@@ -41,11 +40,10 @@ describe('User routes', () => {
     //   expect(res.body.price).to.equal(200)
     // })
 
-
     // it('DELETE /api/users/', async () => {
     //   const res = await request(app)
     //     .del('/api/users/1')
     //     .expect(200)
     // })
-  }) // end describe('/api/users')
-}) // end describe('User routes')
+  }); // end describe('/api/users')
+}); // end describe('User routes')
