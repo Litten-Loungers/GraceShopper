05baaccc30c041a5569c386cc3bd5a64bf9e48e6
Jonathan Donadio <jondonadio@gmail.com>
2022-03-05 12:56:07 -0500

    Edit form functional for logged in users

--- client/components/EditProductForm.js
+++ client/components/EditProductForm.js
@@ -1,66 +1,51 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateProduct } from '../store';
 
-class EditProjectForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+export default function EditProductForm() {
+  // pull singleProduct from store
+  const singleProduct = useSelector((state) => state.singleProduct);
+  // set local state
+  const [product, setProduct] = useState(singleProduct);
+  //initialize dispatch function
+  const dispatch = useDispatch();
+
+  const { name, price, description, imageURL, quantity, available } = product;
+
+  function handleChange(evt) {
+    setProduct({
+      [evt.target.name]: evt.target.value,
+    });
   }
 
-  handleChange() {
-    console.log('test');
-  }
-
-  handleSubmit(evt) {
+  function handleSubmit(evt) {
     evt.preventDefault();
-    console.log('test');
+    dispatch(updateProduct(singleProduct.id, product));
   }
 
-  render() {
-    const { name, price, description, imageURL, quantity, available } =
-      this.props;
-    const { handleSubmit, handleChange } = this;
+  return (
+    <div className="edit-product-form">
+      <form id="edit-product-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name </label>
+        <input name="name" onChange={handleChange} value={name} />
 
-    return (
-      <div className="edit-product-form">
-        <form id="edit-product-form" onSubmit={handleSubmit}>
-          <label htmlFor="name">Name </label>
-          <input name="name" onChange={handleChange} value={name} />
+        <label htmlFor="price">Price </label>
+        <input name="price" onChange={handleChange} value={price} />
 
-          <label htmlFor="price">Price </label>
-          <input name="price" onChange={handleChange} value={price} />
+        <label htmlFor="description">Description </label>
+        <input name="description" onChange={handleChange} value={description} />
 
-          <label htmlFor="description">Description </label>
-          <input
-            name="description"
-            onChange={handleChange}
-            value={description}
-          />
+        <label htmlFor="imageURL">ImageURL </label>
+        <input name="imageURL" onChange={handleChange} value={imageURL} />
+        <label htmlFor="quantity">Quantity </label>
+        <input name="quantity" onChange={handleChange} value={quantity} />
+        <label htmlFor="available">Available </label>
+        <input name="available" onChange={handleChange} value={available} />
 
-          <label htmlFor="imageURL">ImageURL </label>
-          <input name="imageURL" onChange={handleChange} value={imageURL} />
-          <label htmlFor="quantity">Quantity </label>
-          <input name="quantity" onChange={handleChange} value={quantity} />
-          <label htmlFor="available">Available </label>
-          <input name="available" onChange={handleChange} value={available} />
-
-          <button type="submit" className="submit-btn">
-            Save Changes
-          </button>
-        </form>
-      </div>
-    );
-  }
+        <button type="submit" className="submit-btn">
+          Save Changes
+        </button>
+      </form>
+    </div>
+  );
 }
-
-const mapStateToProps = ({ project }) => ({
-  project,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  updateProject: (project) => dispatch(updateProject(project)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(EditProjectForm);
