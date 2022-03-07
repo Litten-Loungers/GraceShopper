611f4a7f895698b707501eb44c296bc8eaa06efa
Lucas McGill <lmlucasmc8@gmail.com>
2022-03-07 16:58:20 -0500

    only renders manage products if admin

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -2,22 +2,33 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-export const Sidebar = () => {
-  return (
-    <div className='sidebar'>
-      <div>FILTER BY TYPE</div>
-      <div>
-        <Link to='/cart'>IN CART</Link>
+export class Sidebar extends React.Component {
+  render() {
+    const { isAdmin } = this.props;
+    return (
+      <div className="sidebar">
+        <div>FILTER BY TYPE</div>
+        <div>
+          <Link to="/cart">IN CART</Link>
+        </div>
+        <div>
+          <Link to="/checkout">CHECK OUT</Link>
+        </div>
+        {isAdmin ? (
+          <div>
+            {' '}
+            <Link to="/admin/products">MANAGE PRODUCTS</Link>
+          </div>
+        ) : (
+          <div />
+        )}
       </div>
-      <div>
-        <Link to='/checkout'>CHECK OUT</Link>
-      </div>
-      <div>
-        {' '}
-        <Link to='/admin/products'>MANAGE PRODUCTS</Link>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default connect()(Sidebar);
+const mapStatetoProps = (state) => ({
+  isAdmin: state.auth.type === 'ADMIN',
+});
+
+export default connect(mapStatetoProps)(Sidebar);
