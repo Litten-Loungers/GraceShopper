b4b309abef8beae34ad2c199a078c2b8327cef94
Lucas McGill <lmlucasmc8@gmail.com>
2022-03-07 18:06:08 -0500

    commented out unused routes as of yet

--- server/api/orders.js
+++ server/api/orders.js
@@ -6,45 +6,45 @@ const {
 module.exports = router;
 
 //GET all orders
-router.get('/', async (req, res, next) => {
-  try {
-    const orders = await Order.findAll();
-    res.json(orders);
-  } catch (err) {
-    next(err);
-  }
-});
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const orders = await Order.findAll();
+//     res.json(orders);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 //POST single order
-router.post('/', async (req, res, next) => {
-  try {
-    res.status(201).send(await Order.create(req.body));
-  } catch (error) {
-    next(error);
-  }
-});
+// router.post('/', async (req, res, next) => {
+//   try {
+//     res.status(201).send(await Order.create(req.body));
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 //DELETE single order
-router.delete('/:orderId', async (req, res, next) => {
-  const id = Number(req.params.orderId);
-  try {
-    await Order.destroy({
-      where: {
-        id: id,
-      },
-    });
-    res.send(204);
-  } catch (error) {
-    next(error);
-  }
-});
+// router.delete('/:orderId', async (req, res, next) => {
+//   const id = Number(req.params.orderId);
+//   try {
+//     await Order.destroy({
+//       where: {
+//         id: id,
+//       },
+//     });
+//     res.send(204);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 //GET single order
-router.get('/:orderId', async (req, res, next) => {
-  try {
-    const order = await Order.findByPk(req.params.orderId);
-    res.json(order);
-  } catch (err) {
-    next(err);
-  }
-});
+// router.get('/:orderId', async (req, res, next) => {
+//   try {
+//     const order = await Order.findByPk(req.params.orderId);
+//     res.json(order);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
