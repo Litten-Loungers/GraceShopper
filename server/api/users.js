554b656f04b44aa1343d57141cc9c6427a18de21
Lucas McGill <lmlucasmc8@gmail.com>
2022-03-07 17:06:34 -0500

    can only view all users if an admin

--- server/api/users.js
+++ server/api/users.js
@@ -1,24 +1,39 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const {
   models: { User },
-} = require("../db");
+} = require('../db');
 module.exports = router;
 
-router.get("/", async (req, res, next) => {
+const requireToken = async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      attributes: ["id", "username"],
-    });
-    res.json(users);
+    const token = req.headers.authorization;
+    const user = await User.findByToken(token);
+    req.user = user.dataValues;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+router.get('/', requireToken, async (req, res, next) => {
+  try {
+    if (req.user.type === 'ADMIN') {
+      const users = await User.findAll({
+        attributes: ['id', 'username'],
+      });
+      res.json(users);
+    } else {
+      throw new Error();
+    }
   } catch (err) {
     next(err);
   }
 });
 
-router.get("/:userId", async (req, res, next) => {
+router.get('/:userId', async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.userId, {
-      attributes: ["id", "username"],
+      attributes: ['id', 'username'],
     });
     res.json(user);
   } catch (err) {
