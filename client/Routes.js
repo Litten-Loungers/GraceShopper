64d296c0b951a337ee9aadbcee758b79bc12f305
Lucas McGill <lmlucasmc8@gmail.com>
2022-03-07 17:18:56 -0500

    made sure users cant set their type to admin

--- client/Routes.js
+++ client/Routes.js
@@ -31,7 +31,7 @@ class Routes extends Component {
         <Sidebar />
         {isLoggedIn ? (
           <Switch>
-            <Route path='/users' component={Users} />
+            <Route path='/admin/users' component={Users} />
             <Route path='/checkout' component={Checkout} />
             <Route path='/cart' component={Order} />
             <Route path='/testcartwidget' component={CartWidget} />
