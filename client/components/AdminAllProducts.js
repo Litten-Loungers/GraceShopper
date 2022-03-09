ec7a1d8b8a6fd4554eb23e357688b36b6238837b
Jonathan Donadio <jondonadio@gmail.com>
2022-03-08 21:51:24 -0500

    initial refactoring

--- client/components/AdminAllProducts.js
+++ client/components/AdminAllProducts.js
@@ -1,83 +1,54 @@
 import axios from 'axios';
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchProducts, addItemToCart } from '../store';
+import { fetchProducts, destroyProduct } from '../store';
 
-class AdminAllProducts extends React.Component {
-  constructor() {
-    super();
-    this.removeProduct = this.removeProduct.bind(this);
-  }
+export default function AdminAllProducts() {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products);
 
-  async removeProduct(productId) {
-    const token = window.localStorage.getItem('token');
-    await axios.delete(`/api/products/${productId}`, {
-      headers: { authorization: token },
-    });
-    await this.props.fetchProducts();
-  }
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
 
-  async componentDidMount() {
-    await this.props.fetchProducts();
-  }
-  render() {
-    const { products } = this.props;
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <h3>Admin / Manage Products: </h3>
-        </div>
-        <div>
-          <Link to={'/admin/products/add'}>
-            <button>Add Product</button>
-          </Link>
-        </div>
-        <hr />
-        <div className='all-products'>
-          {products
-            .sort((x, y) => {
-              return x.id - y.id;
-            })
-            .map((product) => {
-              return (
-                <div key={product.id}>
-                  <h2>Name: {product.name}</h2>
-                  <img src={product.imageURL} />
-                  <p>Price: $ {product.price}</p>
-                  <p>{product.description}</p>
-                  <p>Quantity: {product.quantity}</p>
-                  <Link to={`/admin/products/${product.id}/edit`}>
-                    <button>Edit</button>
-                  </Link>
-                  <button
-                    type='button'
-                    onClick={() => this.removeProduct(product.id)}
-                  >
-                    Remove
-                  </button>
-                </div>
-              );
-            })}
-        </div>
+        <h3>Admin / Manage Products: </h3>
       </div>
-    );
-  }
+      <div>
+        <Link to={'/admin/products/add'}>
+          <button>Add Product</button>
+        </Link>
+      </div>
+      <hr />
+      <div className="all-products">
+        {products
+          .sort((x, y) => {
+            return x.id - y.id;
+          })
+          .map((product) => {
+            return (
+              <div key={product.id}>
+                <h2>Name: {product.name}</h2>
+                <img src={product.imageURL} />
+                <p>Price: $ {product.price}</p>
+                <p>{product.description}</p>
+                <p>Quantity: {product.quantity}</p>
+                <Link to={`/admin/products/${product.id}/edit`}>
+                  <button>Edit</button>
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => dispatch(destroyProduct(product.id))}
+                >
+                  Remove
+                </button>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
-
-const mapStateToProps = (state) => {
-  return {
-    products: state.products,
-    userId: state.auth.id,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchProducts: () => dispatch(fetchProducts()),
-    addItemToCart: (userId, productId) =>
-      dispatch(addItemToCart(userId, productId)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(AdminAllProducts);
--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -5,12 +5,18 @@ import history from '../history';
  * ACTION TYPES
  */
 const SET_PRODUCT = 'SET_PRODUCT';
+const DELETE_PRODUCT = 'DELETE_PRODUCT';
 
 /**
  * ACTION CREATORS
  */
 const setProduct = (product) => ({
   type: SET_PRODUCT,
+  product,
+});
+
+const deleteProduct = (product) => ({
+  type: DELETE_PRODUCT,
   product,
 });
 
@@ -24,10 +30,23 @@ export const fetchProduct = (id) => {
   };
 };
 
+export const destroyProduct = (id) => {
+  return async (dispatch) => {
+    const token = window.localStorage.getItem('token');
+    await axios.delete(`/api/products/${id}`, {
+      headers: { authorization: token },
+    });
+    dispatch(deleteProduct(id));
+  };
+};
+
 export default function singleProduct(state = {}, action) {
   switch (action.type) {
     case SET_PRODUCT:
       return action.product;
+    case DELETE_PRODUCT:
+      return action.product;
+
     default:
       return state;
   }
